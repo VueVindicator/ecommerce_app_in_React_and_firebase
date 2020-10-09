0de46f3c2812c7d45e9e20d99dffd9c83fb6cfa8
David <davidrums65@gmail.com>
2020-10-09 16:28:37 +0100

    added menu-items for home pages

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import './menu-item.styles.scss'
-const MenuItem = ({title}) => (
-    <div clasname="menu-item">
+const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}/${linkUrl}`)}>
+        <div style={{backgroundImage: `url(${imageUrl})`}} className="background-image"/>
         <div className="content">
-            <h1 classNametitle>{title}</h1>
-            <span classNametitle>Shop now</span>
+            <h1 className="title">{title.toUpperCase()}</h1>
+            <span className="subtitle">Shop now</span>
         </div>
     </div>
 )
-export default MenuItem
+
+export default withRouter(MenuItem)
