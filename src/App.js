9737318faebeaea2bf99618aea460b0e558992cd
David <davidrums65@gmail.com>
2020-10-09 17:32:37 +0100

    added routes for sign in and sign up pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndRegister from './components/signin-and-register/Signin-and-register'
 
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
           <Route exact path="/shop" component={ShopPage}></Route>
+          <Route exact path="/signin" component={SignInAndRegister}></Route>
         </Switch>
       </div>
     )
